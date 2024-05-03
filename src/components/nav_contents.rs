dd81848e52fab561acd2b37c8a0b6728eaf7594d
Frank Femia <femiaf13@gmail.com>
2024-05-02 22:52:09 -0400

    Sloppy but it's good enough

--- src/components/nav_contents.rs
+++ src/components/nav_contents.rs
@@ -6,30 +6,59 @@ use leptos::*;
 pub fn NavContents(
     vertical: bool
 ) -> impl IntoView {
+
     view! {
-        <Link href="/about">
-            <h3>About</h3>
-        </Link>
-        <Link href="/">
-            <h3>Resume</h3>
-        </Link>
-        <LinkExt
-            href="https://www.linkedin.com/in/frank-femia-iii"
-            target=LinkExtTarget::Blank
-            // style="margin-top: 15px;"
-        >
-            <Icon
-                id="linkedin-icon"
-                class="header-icon"
-                icon=icondata::BsLinkedin
-            />
-        </LinkExt>
-        <LinkExt 
-            href="https://github.com/femiaf13" 
-            target=LinkExtTarget::Blank
-            // style="margin-top: 15px;"
-        >
-            <Icon id="github-icon" class="header-icon" icon=icondata::BsGithub/>
-        </LinkExt>
+        <Show
+            when=move || !vertical
+            fallback=move || view! {
+                <Link href="/about">
+                    <h3>About</h3>
+                </Link>
+                <Link href="/">
+                    <h3>Resume</h3>
+                </Link>
+                <LinkExt
+                    href="https://www.linkedin.com/in/frank-femia-iii"
+                    target=LinkExtTarget::Blank
+                    style="margin-top: 15px;"
+                >
+                    <Icon
+                        id="linkedin-icon"
+                        class="header-icon"
+                        icon=icondata::BsLinkedin
+                    />
+                </LinkExt>
+                <LinkExt 
+                    href="https://github.com/femiaf13" 
+                    target=LinkExtTarget::Blank
+                    style="margin-top: 15px;"
+                >
+                    <Icon id="github-icon" class="header-icon" icon=icondata::BsGithub/>
+                </LinkExt>
+            }
+        >     
+            <Link href="/about">
+                <h3>About</h3>
+            </Link>
+            <Link href="/">
+                <h3>Resume</h3>
+            </Link>
+            <LinkExt
+                href="https://www.linkedin.com/in/frank-femia-iii"
+                target=LinkExtTarget::Blank
+            >
+                <Icon
+                    id="linkedin-icon"
+                    class="header-icon"
+                    icon=icondata::BsLinkedin
+                />
+            </LinkExt>
+            <LinkExt 
+                href="https://github.com/femiaf13" 
+                target=LinkExtTarget::Blank
+            >
+                <Icon id="github-icon" class="header-icon" icon=icondata::BsGithub/>
+            </LinkExt>
+        </Show>
     }
 }
